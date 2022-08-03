2396aaa810d40434d1124d764a9bfe5951bf42cf
Aleksandr <i@afeklin.ru>
2022-08-03 22:58:26 +0300

    Example 005 _Else

---
+++ Example005_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="света")
+{
+     Console.WriteLine("Ура, это СВЕТА!");
+}
+else
+{
+     Console.Write("Привет, ");
+     Console.WriteLine(username);
+
+}
